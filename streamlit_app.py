6db082be92a3f31935796b90a4d1390ff950a07d
EvidentTest <thomas.peeden.ext@evidentscientific.com>
2026-06-25 18:00:33 +0800

    new section to display smoothiefroot nutrition information
    new section to display smoothiefroot nutrition information

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,11 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw: ")
 st.write(
   """Choose the fruits you want in your custom Smoothie!
   """)
-
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your smoothie will be', name_on_order)
@@ -45,3 +43,8 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success('Your Smoothie is ordered!', icon="✅")
+
+#new section to display smoothiefroot nutrition information
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
